b17712598afe89dded75991118d4aa652b80d618
Guille <g.mirandagaleote@gmail.com>
2023-11-19 23:09:49 +0100

    Config server and api

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,28 @@
-const x = 22;
-console.log(x);
+import { createServer} from 'http'
+import 'dotenv/config'
+
+
+const PORT = process.env.PORT || 3030;
+
+const server = createServer((req, res)=>{
+
+  const url = new URL(req.url as string, `http://${req.headers.host}`);
+
+  res.setHeader('Content-type', 'text/html');
+  res.write('<h1>Knowledge</h1>');
+
+  if (url.pathname.toLowerCase() === '/knowledge') {
+    res.write('<h2>List of Knowledge</h2>');
+  } else {
+    server.emit('error', new Error ('not today'));
+  }
+
+  res.end();
+
+})
+
+server.listen(PORT);
+
+server.on('error', (error) => {
+  console.log(error.message);
+});
---
+++ src/models/knowledge.ts
@@ -0,0 +1,9 @@
+export type KnowledgeStructure ={
+  
+  id: number;
+  name: string;
+  week: number;
+  learned: boolean;
+  image: string;
+  
+}
